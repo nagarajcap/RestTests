8b19f45d4f5590cd7efcb460db51d1910009b661
nagarajcap <164017641+nagarajcap@users.noreply.github.com>
2024-11-01 14:43:57 +0530

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+GITHUB_REPO="Nagarajcts/RestTests"  
+GITHUB_TOKEN="dGpwS2AIpepeTGx5ZiapX2muZtgs1025YeLR"    
+GITHUB_WORKFLOW="FT_RestTests.yml"      
+REF="main"                             
+
+# Trigger GitHub Action via GitHub API
+curl -X POST \
+  -H "Accept: application/vnd.github+json" \
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  -H "Authorization: Bearer ghp_$GITHUB_TOKEN" \
+  https://api.github.com/repos/$GITHUB_REPO/actions/workflows/$GITHUB_WORKFLOW/dispatches \
